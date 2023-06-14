f84c46c0657101ed97be1527f70d31ff2f64b65f
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-06-15 01:30:30 +0530

    init project chat guru

---
+++ app.js
@@ -0,0 +1,16 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res, next) => {
+  res.render("public/index");
+});
+
+app.listen("3000");
