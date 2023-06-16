ce4a4d6271ca5eb4841c48e67253e0b7050c288b
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-06-16 11:01:08 +0530

    fixed typing link text show in latest element

--- public/js/script.js
+++ public/js/script.js
@@ -8,6 +8,17 @@ const copy = document.querySelectorAll(".copy");
 const code = document.querySelectorAll(".code");
 const public = document.querySelector(".chat-section");
 
+// cookie function
+
+function set_cookie(name, value) {
+  document.cookie = name + "=" + value + "; Path=/;";
+}
+function delete_cookie(name) {
+  document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+}
+
+// when page relode it always go to last div
+
 if (public) {
   window.addEventListener("load", () => {
     public.scrollIntoView({
@@ -17,15 +28,25 @@ if (public) {
   });
 }
 
+// regenerate button loder on
+
 if (reButton !== null) {
   reButton.addEventListener("click", loders);
 }
+
+// submit buton loder on
 button.addEventListener("click", loders);
 
+// function to load loder on click submit button and regenerate button
+// also set cookies for farther use
+// add question and loging dynamic when sumit button click
 function loders() {
   if (input.value.length > 0) {
+    set_cookie("newOutput", "true");
     button.classList.toggle("hidden");
     loder.classList.toggle("hidden");
+
+    // add question and loging dynamic when sumit button click
     if (public) {
       public.innerHTML += `<div class="chat-by-public chat">
       <p class="question">${input.value}</p>
@@ -46,6 +67,8 @@ function loders() {
 </div> </p>
      <img class="logo-gpt" src="/images/chat.jpg" alt="" />
     </div>`;
+
+      //after sumit button page view go to last div
       public.scrollIntoView({
         block: "end",
         inline: "nearest",
@@ -54,6 +77,8 @@ function loders() {
   }
 }
 
+// when input key preess loder go hide and button come on
+
 input.addEventListener("keypress", () => {
   if (button.classList.contains("hidden")) {
     button.classList.remove("hidden");
@@ -61,60 +86,49 @@ input.addEventListener("keypress", () => {
   }
 });
 
-if (answer) {
-  answer.forEach((a) => {
-    const value = a.textContent.replaceAll("\n", "<br>");
-    a.innerHTML = value;
-  });
-}
-
+// when windows loading done it check my decided cooke is present or not if present then grab latest
+// elemnt text content and show it slowly word by word (like some one typing)
+// then delete the cooker because when page relode any no input paresent i dont want show smae typing animation evertime
 window.addEventListener("load", () => {
+  let cookie = document.cookie.split(" ")[0].split("=");
+  let name = cookie[0];
+  let value = cookie[1];
   let list = [];
   answer.forEach((a) => {
-    const text = a;
     list.push(a);
   });
-  let text = list.slice(-1)[0].textContent;
-  let index = 0;
+  let text = list.slice(-1)[0].textContent.replaceAll("\n", "<br>");
+  let index = 10;
+
+  // then function print text one by one
   function type() {
     if (index < text.length) {
       list.slice(-1)[0].innerHTML =
         text.slice(0, index) + '<span class="blinking-cursor">|</span>';
       index++;
-      setTimeout(type, Math.random() * 150 + 50);
+      setTimeout(type, Math.random() * 50 + 10);
     } else {
       list.slice(-1)[0].innerHTML =
         text.slice(0, index) + '<span class="blinking-cursor">|</span>';
     }
+    delete_cookie(name);
   }
-  type();
+
+  // check if cooke is corrent
+  if (name == "newOutput") {
+    type();
+  }
+
+  // replce all elemnt textcontain(\n) with <br> to show it next line in html
+  if (answer) {
+    answer.forEach((a) => {
+      const value = a.textContent.replaceAll("\n", "<br>");
+      a.innerHTML = value;
+    });
+  }
 });
 
-async function btn() {
-  button.addEventListener("click", () => {
-    let index = 0;
-    window.addEventListener("load", () => {
-      answer.forEach((a) => {
-        const text = a;
-        console.log(a);
-        // if (index < text.length) {
-        //   console.log(a[-1]);
-
-        //   a[-1].innerHTML =
-        //     text.slice(0, index) + '<span class="blinking-cursor">|</span>';
-        //   index++;
-        //   setTimeout(type, Math.random() * 150 + 50);
-        // } else {
-        //   a[-1].innerHTML =
-        //     text.slice(0, index) + '<span class="blinking-cursor">|</span>';
-        // }
-      });
-    });
-  });
-}
-
-btn();
-
+// copy content from element
 copy.forEach((e) => {
   e.addEventListener("click", (f) => {
     let text = "";
@@ -135,6 +149,8 @@ copy.forEach((e) => {
     copyContent();
   });
 });
+
+// take code from input and then split it code and other comment and view them dynamically
 
 code.forEach((c) => {
   const list = c.value.split("```");
