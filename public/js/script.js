0b97095729b53090a120086769fcdec94d2cccd9
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-06-19 00:17:49 +0530

    update pwa settings

--- public/js/script.js
+++ public/js/script.js
@@ -227,3 +227,23 @@ code.forEach((c) => {
 });
 
 //language-bash
+
+let defferedPrompt;
+const addbtn = document.querySelector(".btnk");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  defferedPrompt = event;
+  addbtn.style.display = "block";
+});
+
+addbtn.addEventListener("click", (event) => {
+  defferedPrompt.prompt();
+
+  defferedPrompt.userChoice.then((choice) => {
+    if (choice.outcome === "accepted") {
+      console.log("user accepted the prompt");
+    }
+    defferedPrompt = null;
+  });
+});
