25c82c31d55ff3653101eb7cb7a0ffd28ea6ffc9
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-06-18 17:47:57 +0530

    chat section multiple api accept done

--- controller/public.js
+++ controller/public.js
@@ -1,6 +1,28 @@
 require("dotenv").config();
-const { validationResult } = require("express-validator");
+const { validationResult, Result } = require("express-validator");
 const axios = require("axios");
+const User = require("../model/user");
+const { response } = require("express");
+const user = require("../model/user");
+const nodeMailer = require("nodemailer");
+// const user = new User({
+//   apikeyindex: 0,
+// });
+// user
+//   .save()
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((err) => console.log(err));
+
+const transporter = nodeMailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.USER_ID,
+    pass: process.env.PASSWORD,
+  },
+});
+
 let total = 28896;
 async function api() {
   const options = {
@@ -42,7 +64,7 @@ exports.getChatIndex = (req, res, next) => {
   res.render("public/chat", {
     answer: [
       {
-        question: "Hi! ChatGuru",
+        question: "Hi! Chat Sonic",
         answer: `Hello there! How can I assist you today?`,
       },
     ],
@@ -76,14 +98,20 @@ exports.postChat = (req, res, next) => {
   }
   req.session.message.push({ role: "user", content: que });
 
-  async function apiCall() {
+  async function apiCall(indexApi) {
+    console.log(indexApi);
+    let api;
+    if (indexApi >= 0) {
+      api = process.env.API_KEY.split(",")[indexApi];
+      console.log(api);
+    }
     const options = {
       method: "POST",
       url: "https://openai80.p.rapidapi.com/chat/completions",
       headers: {
         "Accept-Encoding": "gzip,deflate,compress",
         "content-type": "application/json",
-        "X-RapidAPI-Key": process.env.API_KEY,
+        "X-RapidAPI-Key": api,
         "X-RapidAPI-Host": "openai80.p.rapidapi.com",
       },
       data: {
@@ -92,45 +120,98 @@ exports.postChat = (req, res, next) => {
       },
     };
 
-    try {
-      const response = await axios.request(options);
-      const reply = response.data.choices[0].message.content;
-      console.log(reply);
-      total += Number(response.data.usage.total_tokens);
-      console.log(total);
-
-      if (reply.includes("```")) {
-        req.session.answer.push({
-          question: que,
-          answer: reply,
-          isCode: true,
-        });
-      } else {
-        req.session.answer.push({
-          question: que,
-          answer: reply,
-          isCode: false,
-        });
-      }
-
-      res.render("public/chat", {
-        answer: req.session.answer,
-      });
-    } catch (error) {
-      res.render("public/chat", {
-        answer: [
-          {
+    axios
+      .request(options)
+      .then((response) => {
+        const reply = response.data.choices[0].message.content;
+        console.log(reply);
+        total += Number(response.data.usage.total_tokens);
+        console.log(total);
+        if (reply.includes("```")) {
+          req.session.answer.push({
             question: que,
-            answer: "Something went wrong. Please try again later.",
-          },
-        ],
-      });
-      let errorData = error.response.data.message;
-      console.log(errorData);
-    }
-  }
+            answer: reply,
+            isCode: true,
+          });
+        } else {
+          req.session.answer.push({
+            question: que,
+            answer: reply,
+            isCode: false,
+          });
+        }
 
-  apiCall();
+        res.render("public/chat", {
+          answer: req.session.answer,
+        });
+      })
+      .catch((error) => {
+        res.render("public/chat", {
+          answer: [
+            {
+              question: "What's wrong Chat Sonic?",
+              answer:
+                "Sorry we faced some api issue please wait for a moment .It will be fixed automatically.Please try again",
+            },
+          ],
+        });
+
+        let errorData = error.response.data.message;
+
+        if (
+          errorData.includes(
+            "You have exceeded the MONTHLY quota for Tokens on your current plan"
+          )
+        ) {
+          User.findById("648edc6b1f324c954afc65d7").then((user) => {
+            let userApiIndex = user.apikeyindex + 1;
+            user.apikeyindex = userApiIndex;
+            user
+              .save()
+              .then((result) => {
+                const mailOption = {
+                  from: process.env.USER_ID,
+                  to: process.env.TO_USER_ID,
+                  subject: "Welcome To Shop",
+                  html: `<html><body style="width : 90%; margin : auto ; background-color :#000000d9,padding : 15px ;">
+                
+                <div style="width : 95% ;height : 90%; text-align : center; margin : 12px auto ; background-color : #0c0921; padding:1rem">
+                <h1 style="color : green">Hi Your Api limit is end. We call a new api . please add more api</h1>
+
+                </div>
+                
+                </body></html>`,
+                };
+                return transporter.sendMail(mailOption);
+              })
+              .then((response) => {
+                console.log(response);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          });
+        } else {
+          console.log("error");
+          res.render("public/chat", {
+            answer: [
+              {
+                question: "what's wrong Chat Sonic?",
+                answer: "Something went wrong. Please try again later",
+              },
+            ],
+          });
+        }
+      });
+  }
+  user
+    .findById("648edc6b1f324c954afc65d7")
+    .then((user) => {
+      apiCall(user.apikeyindex);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postImage = (req, res, next) => {
--- model/user.js
+++ model/user.js
@@ -1,3 +1,11 @@
 const mongoos = require("mongoose");
 
 const Schema = mongoos.Schema;
+
+const userSchema = new Schema({
+  apikeyindex: {
+    type: Number,
+  },
+});
+
+module.exports = mongoos.model("User", userSchema);
