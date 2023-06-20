fdde1ab14f3f6c2d7fdc1bb752036f9c7a540181
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-06-20 13:10:20 +0530

    check pwa prompt issue

--- public/js/script.js
+++ public/js/script.js
@@ -280,3 +280,23 @@ share.forEach((s) => {
     }
   });
 });
+
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+installButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+});
