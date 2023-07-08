b5146b7f48fa90dbdaf1ce505964755556fa3883
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2023-07-09 03:06:54 +0530

    update user model

--- model/user.js
+++ model/user.js
@@ -1,51 +1,54 @@
 const mongoos = require("mongoose");
-
+const User = require("./user");
 const Schema = mongoos.Schema;
 
 const userSchema = new Schema({
-  conversation : {
-    message : [{
-      type:Object
-    }],
-    answer : [
+  conversation: {
+    message: [
       {
-        type:Object
-      }
-    ]
+        type: Object,
+        unique: false,
+      },
+    ],
+    answer: [
+      {
+        type: Object,
+        unique: false,
+      },
+    ],
   },
-  ipAddress : {
-    type:String
+  ipAddress: {
+    type: String,
   },
-  imagesection : [{
-    type : Object
-  }]
+  imagesection: [
+    {
+      type: Object,
+      unique: false,
+    },
+  ],
 });
 
-userSchema.methods.addMessage = function(message){
+userSchema.methods.addMessage = function (message) {
   const oldmessage = this.conversation.message;
   let updatedCart = [...oldmessage];
-  updatedCart.push(message)
-  this.conversation.message = updatedCart
+  updatedCart.push(message);
+  this.conversation.message = updatedCart;
   return this.save();
-}
+};
 
-userSchema.methods.addAnswer = function(answer)
-{
+userSchema.methods.addAnswer = function (answer) {
   const oldanswer = this.conversation.answer;
   let updatedAnswer = [...oldanswer];
   updatedAnswer.push(answer);
   this.conversation.answer = updatedAnswer;
   return this.save();
-} 
+};
 
-
-userSchema.methods.addToImageSection = function(imageobj){
+userSchema.methods.addToImageSection = function (imageobj) {
   const imageOldQuery = this.imagesection;
   let updatedImageSection = [...imageOldQuery];
   updatedImageSection.push(imageobj);
-  this.imagesection = updatedImageSection;
   return this.save();
-}
-
+};
 
 module.exports = mongoos.model("User", userSchema);
